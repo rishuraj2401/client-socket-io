42b5c1cc6bcda9faee059bf6bebab403033bbb52
rishuraj2401 <rishuraj2401sinha@gmail.com>
2023-04-06 14:29:17 +0530

    6apr

--- client/src/App.js
+++ client/src/App.js
@@ -5,19 +5,10 @@ import Home from "./Home";
 import "antd/dist/antd.css";
 import "font-awesome/css/font-awesome.min.css";
 import Footer from "./components/Footer/Footer";
-import Editor from "./components/Video/Editor";
-import VideoContext from "./context/VideoContext";
 const App = () => {
  
   return (
     <>
-    {/* <Editor
-          language="xml"
-          displayName="HTML"
-          value={message}
-          onChange={setMessage}
-        /> */}
-        {/* {<Editor/>} */}
     <Router>
       <Switch>
         <Route path="/" component={Home} />
